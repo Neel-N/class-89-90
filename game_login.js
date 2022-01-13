8a47eb8fd4a6fbaae9095caeb37f3cd1b6069b85
Neel-N <86572483+Neel-N@users.noreply.github.com>
2022-01-12 18:09:26 -0800

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -4,5 +4,5 @@ player2_name=document.getElementById("player2_name_input").value;
  localStorage.setItem("player1_name",player1_name);
  localStorage.setItem("player2_name",player2_name);
 
-window.location="game_page.hmtl";
+window.location="game_page.html";
 }
